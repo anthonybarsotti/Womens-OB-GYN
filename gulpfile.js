39ab74f5de291697f8f7f6b691f77efe800edaf2
Anthony Barsotti <anthonydbarsotti@gmail.com>
2014-03-23 16:52:51 -0400

    Added jquery, bootstrap-sass and gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+var gulp = require('gulp'),
+sass = require('gulp-ruby-sass'),
+concat = require('gulp-concat'),
+minifycss = require('gulp-minify-css'),
+rename = require('gulp-rename'),
+uglify = require('gulp-uglify'),
+livereload = require('gulp-livereload');
+
+gulp.task('styles', function() {
+	return gulp.src('stylesheets/*.scss')
+	.pipe(sass({ style: 'expanded' }))
+	.pipe(gulp.dest('css'))
+	.pipe(rename({suffix: '.min'}))
+	.pipe(minifycss())
+    .pipe(concat('all.min.css'))
+	.pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('scripts', function() {
+	return gulp.src('javascripts/*.js')
+	.pipe(rename({suffix: '.min'}))
+	.pipe(uglify())
+	.pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('watch', function() {
+  gulp.watch('stylesheets/**', ['styles']);
+  gulp.watch('javascripts/**', ['scripts']);
+  var server = livereload();
+  gulp.watch('dist/**').on('change', function(file) {
+      server.changed(file.path);
+  });
+});
+
+gulp.task('default', ['styles', 'scripts', 'watch']);
